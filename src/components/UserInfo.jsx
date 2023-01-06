a7e8b0b6ff32023ea97575cd3d45e160b657847b
Medetalieva Dilnaz <dilnazmedetalieva422@gmail.com>
2023-01-06 09:07:27 +0600

    add h3

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -5,6 +5,7 @@ import React from "react";
 function UserInfo(props){
     return(
      <div className="userInfo">
+        <h3>Hello</h3>
         <Avatar authorInfo={props.author}/>
         <div className="userInfo-name">{props.author.name}</div>
      </div>
